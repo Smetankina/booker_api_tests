42bf58defdc0dbbf90a9324b87e806cc9e4374b6
Anna Smetankina <smatankina.anna.a@gmail.com>
2024-06-13 21:36:01 -0700

    Added Allure and DisplayName annotation for handles

--- src/test/java/com/mycompany/tests/getBookingIdsPositiveTests.java
+++ src/test/java/com/mycompany/tests/getBookingIdsPositiveTests.java
@@ -1,20 +1,19 @@
 package com.mycompany.tests;
 
 import com.mycompany.utils.client.BookingClient;
-
+import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import java.io.IOException;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
 
-
-public class getBookingIdsPositive {
+public class getBookingIdsPositiveTests {
     BookingClient bookingClient = new BookingClient();
 
     @DisplayName("Check status code 200 getBookingIdsPositive")
+    @Description("Check status code 200 getBookingIdsPositive")
     @Test
     public void getBookingIdsReturn200OK() throws IOException {
         bookingClient.getBookingIds()
@@ -22,21 +21,22 @@ public class getBookingIdsPositive {
                 .assertThat()
                 .statusCode(200);
     }
+
     @DisplayName("Check status Line OK getBookingIdsPositive")
+    @Description("Check status Line OK getBookingIdsPositive")
     @Test
-    public void getBookingIdsStatusLineOk() throws IOException{
+    public void getBookingIdsStatusLineOk() throws IOException {
         bookingClient.getBookingIds()
                 .then()
                 .statusLine(containsString("OK"));
     }
 
     @DisplayName("Check Content Type getBookingIdsPositive")
+    @Description("Check Content Type getBookingIdsPositive")
     @Test
-    public void getBookingIdsVerifyContentType() throws IOException{
+    public void getBookingIdsVerifyContentType() throws IOException {
         bookingClient.getBookingIds()
                 .then()
                 .contentType("application/json; charset=utf-8");
     }
-
-
 }
